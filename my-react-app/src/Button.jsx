6416244a7876b7a53cd59f23b44a31c853094413
Rajnish <rajnishrajak82@gmail.com>
2025-06-02 15:09:34 +0545

    Using External Buttom

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,3 +1,12 @@
+//How To Style React Compnnents With Css
+
+//(not including external frameworks or preprocessors)
+
+//1. EXTERNAL 
+//2. MODULES
+//3. INLINE
+
+import './Button.jsx'
 function Button(){
     return(<button className = "button">Click me</button>);
 }
